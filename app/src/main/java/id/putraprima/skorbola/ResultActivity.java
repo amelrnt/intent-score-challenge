4edf9c494c52d400da893faecccaf9d06631e1a2
amelrnt <ochaarn@gmail.com>
2020-02-28 10:19:07 +0700

    finish result activity

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,19 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+    private TextView tvWinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        tvWinner = findViewById(R.id.tvWinner);
+        Bundle extras = getIntent().getExtras();
+
+        tvWinner.setText("Selamat "+extras.getString(MatchActivity.WINNER_KEY));
     }
 }
